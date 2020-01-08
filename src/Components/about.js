e4bf279ecb99f6cff7df713f3108751c5619b233
liquidsword <audenrobertson@gmail.com>
2020-01-08 18:17:45 -0500

    about page mock up

--- src/Components/about.js
+++ src/Components/about.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import '../about.css';
 
 class About extends React.Component {
   render() {
     return (
-      <div>
-        <p>This scheduler is here to help keep you and your barber organized. 
+      <>
 
+        <p>This scheduler is here to help keep you and your barber organized.
         </p>
+        <div id="about-image">
       </div>
+      </>
       );
     };
 };
