3f2fb93990ba83a10522464a0c06efd45334da64
liquidsword <audenrobertson@gmail.com>
2020-01-13 00:02:10 -0500

    reconfiguring my frontend
    Deleting unnecessary files. Going bare bones.

--- src/App.js
+++ src/App.js
@@ -10,6 +10,13 @@ import {
 } from "react-router-dom";
 
 class App extends React.Component {
+
+  componentDidMount(){
+    fetch('http://localhoust:3000/api/v1/accounts')
+      .then(response => response.json())
+      .then(date => console.log(data))
+    }
+
   render (){
     return (
        <div className="App"></div>
