41ea2b54c7f49db3b5a33585fbcc09ea85b9d241
liquidsword <audenrobertson@gmail.com>
2020-01-20 14:52:32 -0500

    updated account_list

--- src/components/AccountList.js
+++ src/components/AccountList.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const AccountList = ({props}) => {
+const AccountList = (props) => {
 
   return (
     <div>
-      {props.accountList.map(account => <li key={account.id}>{account.name}</li>)}
+      {props.accountList.map(account => <li key={account.id}>{account.name}> - {account.choice}</li>)}
     </div>
   )
 }
